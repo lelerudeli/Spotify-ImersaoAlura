ac882443cefbcc3312d05b96a1d8c25ee091925c
Letícia <leticiarudeli11@gmail.com>
2024-01-27 20:51:07 -0300

    hope it works #2

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
-console.log('FUNCIONOU ESTA MERDA? ')
-
-
 const searchInput = document.getElementById('search-input'); //ta caçando o input pelo ID dele no html
 const resultArtist = document.getElementById('result-artist');
-const resultPlaylist = document.getElementById('result-presentation');
+const resultPresentation = document.getElementById('result-presentations');
 
-function requestApi (searchTerm) {
+  
+function requestApi(searchTerm) {
                 //parte inicial(busca td)    //buscando o que o usuario está digitando
                 
-    fetch (`http://localhost:3000/artists?name_like=${searchTerm}`)
-        .then((response) => response.json())
-        .then((results) => displayResults(results));  // .then trabalha com promises | programação assincrona
+        fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
+            .then((response) => response.json())
+            .then((result) => displayResults(result));  // .then trabalha com promises | programação assincrona
 }
 
-function displayResults(results) {
-    resultPlaylist.classList.add('hidden');
+function displayResults(result) {
+    hidePresentation();
+    
+    resultPresentation.classList.add('hidden');
     const artistName = document.getElementById('artist-name');
     const artistImage = document.getElementById('artist-img');
 
-    results.forEach(element => {
+    result.forEach(element => {
         artistName.innerText = element.name;
-        artistImage.scr = element.urlImg;
-        
+        artistImage.src = element.urlImg;        
     });
 
     resultArtist.classList.remove('hidden');
 
-    function hidePlaylists() {
-        playlistContainer.classList.add("hidden");
+    function hidePresentation(){
+        resultPresentation.classList.add('hidden');
+    }
+
 }
-}
+
         //evento input e recebe uma outra funcao como parametro 
 document.addEventListener('input', function() { // essa funcao que terá o código com o comportamento que a gente quer
-    const searchTerm = searchInput.value.toLowerCase (); // essa const é o que a gnt vai digitar (recebe o valor do input e deixa td minusculo)    
+    const searchTerm = searchInput.value.toLowerCase(); // essa const é o que a gnt vai digitar (recebe o valor do input e deixa td minusculo)    
         if (searchTerm === '') { // === comparação de valores iguais e do mesmo tipo
-            resultPlaylist.classList.add('hidden'); 
-            resultArtist.classList.remove('hidden');
+            resultArtist.classList.add('hidden');
+            resultPresentation.classList.remove('hidden'); 
+            
             return;
         }
 
         requestApi(searchTerm);
-
 });
